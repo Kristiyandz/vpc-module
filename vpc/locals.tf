5ce289582b1b00ccb928182d061876e0c70a0ebf
Kristiyandz <kristiyandzhakov@gmail.com>
2022-08-28 17:38:21 +0100

    feat: add availability zones

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -3,4 +3,14 @@ locals {
   name = "${var.application}-${var.environment}"
   tags = var.tags
 
+  availability_zones = [for availability in data.aws_availability_zones.available : availability.name]
+
 }
+
+data "aws_availability_zones" "available" {}
+
+data "aws_availability_zones" "available" {
+  for_each = toset(data.aws_availability_zones.available)
+  name     = each.value
+}
+
